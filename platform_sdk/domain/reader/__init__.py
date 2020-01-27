22f03b42af998436795aa8a8e843e620a9c3a973
Lucas Thimoteo <lucas.thimoteo@radixeng.com.br>
2020-01-27 16:55:17 -0300

    Domain Reader query now accepting filter name

--- platform_sdk/domain/reader/__init__.py
+++ platform_sdk/domain/reader/__init__.py
@@ -7,10 +7,10 @@ class DomainReaderApi:
         self.base_uri = params['api_url']
         self.client = HttpClient()
 
-    def get_entities(self, app_name, map_name, _params):
-        uri = self._mount_uri(app_name, map_name, _params)
+    def get_entities(self, app_name, map_name,filter_name, _params):
+        uri = self._mount_uri(app_name, map_name,filter_name, _params)
         print(uri)
         return self.client.get(uri)
 
-    def _mount_uri(self, app_name, map_name, _params):
-        return '{}{}/{}?{}'.format(self.base_uri, app_name, map_name, _params)
+    def _mount_uri(self, app_name, map_name,filter_name, _params):
+        return '{}{}/{}/{}?{}'.format(self.base_uri, app_name, map_name,filter_name, _params)
